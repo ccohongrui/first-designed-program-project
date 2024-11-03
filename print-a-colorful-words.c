f6292ea0a58262ffa06467af53be04970773fc1b
Rui.SICKAMI <ja3847442jimmylin@gmail.com>
2024-11-03 21:06:06 +0800

    Add files via upload

---
+++ print-a-colorful-words.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int main(){
+	printf("\033[31m[KIM]\n\033[0m");
+	printf("\033[31mYou are sunlight and I moon\n\033[0m");
+	printf("\033[31mJoined by the gods of fortune\n\033[0m");
+	printf("\033[31mMidnight and high noon sharing the sky\n\033[0m");
+	printf("\033[31mWe have been blessed , you and I\n\n\033[0m");
+	printf("\033[34m[CHRIS]\n\033[0m");
+	printf("\033[34mYou are here like a mystery\n\033[0m");
+	printf("\033[34mI'm from a world that's so different from all that you are\n\033[0m");
+	printf("\033[34mHow in the light of one night did we come so far?\n\n\033[0m");
+	printf("\033[31m[KIM]\n\033[0m");
+	printf("\033[31mOutside day starts to dawn\n\n\033[0m");
+	printf("\033[34m[CHRIS]\n\033[0m");
+	printf("\033[34mYour moon still floats on high\n\n\033[0m");
+	printf("\033[31m[KIM]\n\033[0m");
+	printf("\033[31mThe birds awake\n\n\033[0m");
+	printf("\033[34m[CHRIS]\n\033[0m");
+	printf("\033[34mThe stars shine too\n\n\033[0m");
+	printf("\033[31m[KIM]\n\033[0m");
+	printf("\033[31mMy hands still shake\n\033[0m");
+	printf("\033[31mSee upcoming pop shows\n\033[0m");
+	printf("\033[31mGet tickets for your favorite artists\n\n\033[0m");
+	printf("\033[31mYou might also like\n\033[0m");
+	printf("\033[31mMy Boy Only Breaks His Favorite Toys\n\033[0m");
+	printf("\033[31mTaylor Swift\n\033[0m");
+	printf("\033[31mWho’s Afraid of Little Old Me?\n\033[0m");
+	printf("\033[31mTaylor Swift\n\033[0m");
+	printf("\033[31mGuilty as Sin?\n\033[0m");
+	printf("\033[31mTaylor Swift\n\n\033[0m");
+	printf("\033[34m[CHRIS]\n\033[0m");
+	printf("\033[34mI reach for you\n\n\033[0m");
+	printf("\033[32m[KIM & CHRIS]\n\033[0m");
+	printf("\033[32mAnd we meet in the sky\n\n\033[0m");
+	printf("\033[31m[KIM]\n\033[0m");
+	printf("\033[31mYou are sunlight and I moon\n\033[0m");
+	printf("\033[31mJoined here\n\033[0m");
+	printf("\033[31mBrightening the sky with the flame of love\n\n\033[0m");
+	printf("\033[32m[KIM & CHRIS]\n\033[0m");
+	printf("\033[32mMade of\n\033[0m");
+	printf("\033[32mSunlight\n\033[0m");
+	printf("\033[32mMoonlight\n\033[0m");
+	return 0;
+}
+
+
+
