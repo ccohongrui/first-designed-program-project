49f0392fe4e3117a0d3a2a46a3aeabe7cc0ede99
Rui.SICKAMI <ja3847442jimmylin@gmail.com>
2024-12-27 04:57:09 +0800

    Add files via upload

---
+++ shapez.h
@@ -0,0 +1,13 @@
+#ifndef SHAPEZ_H
+#define SHAPEZ_H
+#include "encrypt.h"
+
+int miner(int shapez);
+int rotator(int shapez);
+int cutter(int shapez);
+int stacker(int shapezA, int shapezB);
+int mixer(int colorA, int colorB);
+int painter(int shapeA, int color);
+int Target(int task);
+
+#endif // SHAPEZ_H
