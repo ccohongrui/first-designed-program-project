b652cccf52c047b076d36642bb850a95b779dab3
Rui.SICKAMI <ja3847442jimmylin@gmail.com>
2024-12-27 04:55:18 +0800

    Add files via upload

---
+++ use_for_hw0304.c
@@ -0,0 +1,9 @@
+#include "hw0304-1.h"
+#include "hw0304-2.h"
+int main(){
+	printf("Version of recursive\n");
+	printf("%d Action for this activity\n",hanoi_recursive());
+	printf("Version of interative\n");
+	printf("%d Action for this activity\n",hanoi_iterative());
+	return 0;
+}
